65548705988e75c534c95dbf3c4ab32af4108255
jeri-shen <631059305@qq.com>
2017-04-23 21:09:59 +0800

    xiugai

--- src/usr/work/WebActivity.java
+++ src/usr/work/WebActivity.java
@@ -1,6 +1,8 @@
 package usr.work;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -10,7 +12,7 @@ import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class WebActivity extends Activity {
-	
+
 	private WebView webView;
 	private TextView top_title;
 
@@ -19,54 +21,62 @@ public class WebActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_web);
-		
-		
+
 		top_title = (TextView) findViewById(R.id.top_title);
 		top_title.setText(getIntent().getStringExtra("title"));
 		backBtn();
-		
+
 		webView = (WebView) findViewById(R.id.webview);
-		webView.setWebViewClient(new WebViewClient() {
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				// TODO Auto-generated method stub
-				// 返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
-				//view.loadUrl(url);
-				return false;
-			}
-			
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				// TODO Auto-generated method stub
-				super.onPageFinished(view, url);
-			}
-		});
-		
+
+//		webView.setWebViewClient(new WebViewClient() {
+//
+//			@Override
+//			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//				Intent i = new Intent(Intent.ACTION_VIEW);
+//				i.setData(Uri.parse(url));
+//				startActivity(i);
+//				return true;
+//			}
+//		});
+
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.addJavascriptInterface(new JsBridge(), "JsBridge");
-		webView.loadUrl(getIntent().getStringExtra("url")); 
-		
+		webView.loadUrl(getIntent().getStringExtra("url"));
+
 	}
-	
+
 	private void backBtn() {
 		findViewById(R.id.btn_exit).setVisibility(View.VISIBLE);
 		findViewById(R.id.btn_exit).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				finish();
+				if (webView.canGoBack()) {
+					webView.goBack();// 返回上一页面
+				} else {
+					finish();
+				}
 			}
 		});
 	}
-	
-	class JsBridge{
+
+	@Override
+	public void onBackPressed() {
+		if (webView.canGoBack()) {
+			webView.goBack();// 返回上一页面
+		} else {
+			finish();
+		}
+	}
+
+	class JsBridge {
 		@JavascriptInterface
-		public void save(){
-           
-        }
-		
+		public void save() {
+
+		}
+
 		@JavascriptInterface
-		public void updatePwd(){
-			
-        }
+		public void updatePwd() {
+
+		}
 	}
 }
