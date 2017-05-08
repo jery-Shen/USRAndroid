d3f0a024402195a6b36d6ee8ecf569c9886d4519
jeriShen <shenyaojie@greatld.com>
2017-05-08 14:42:44 +0800

    修改

--- src/usr/work/LoginActivity.java
+++ src/usr/work/LoginActivity.java
@@ -4,17 +4,21 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.animation.LinearInterpolator;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import usr.work.utils.HttpUtil;
@@ -28,10 +32,16 @@ public class LoginActivity extends Activity{
 
 
 
-    private TextView top_title;
+   
     private EditText mUserName;
     private EditText mUserPwd;
     private ProgressBar loading;
+    
+    
+
+    private ScrollView mScrollView;
+    private View content;
+    private int keyHeight = 0;      //软件盘弹起后所占高度
     
     private String mUrl = HttpUtil.URL_PRE+"Login";
     
@@ -41,19 +51,16 @@ public class LoginActivity extends Activity{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
-        
-        
-		top_title = (TextView) findViewById(R.id.top_title);
-		top_title.setText("登录");
+
 
         // Set up the login form.
         mUserName = (EditText) findViewById(R.id.username);
-       
-
         mUserPwd = (EditText) findViewById(R.id.userpwd);
-      
-       
         loading = (ProgressBar) findViewById(R.id.loading);
+        mScrollView = (ScrollView) findViewById(R.id.scrollView);
+        content = findViewById(R.id.content);
+        int screenHeight = this.getResources().getDisplayMetrics().heightPixels;
+        keyHeight = screenHeight / 3;
         
         findViewById(R.id.loginbtn).setOnClickListener(new View.OnClickListener() {
 			
@@ -74,7 +81,46 @@ public class LoginActivity extends Activity{
 			}
 		});
         
-        
+        initListener();
+    }
+    
+    private void initListener() {
+        /**
+         * 禁止键盘弹起的时候可以滚动
+         */
+        mScrollView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        mScrollView.addOnLayoutChangeListener(new ViewGroup.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom,
+                                       int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (bottom != 0 && oldBottom != 0 && (oldBottom - bottom > keyHeight)) {
+                    int dist = content.getBottom() - bottom;
+                    if (dist>0){
+                        ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(content, "translationY", 0.0f, -dist);
+                        mAnimatorTranslateY.setDuration(300);
+                        mAnimatorTranslateY.setInterpolator(new LinearInterpolator());
+                        mAnimatorTranslateY.start();
+                      
+                    }
+                   
+                } else if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > keyHeight)) {
+                    if ((content.getBottom() - oldBottom)>0){
+                        ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(content, "translationY", content.getTranslationY(), 0);
+                        mAnimatorTranslateY.setDuration(300);
+                        mAnimatorTranslateY.setInterpolator(new LinearInterpolator());
+                        mAnimatorTranslateY.start();
+                       
+                    }
+                   
+                }
+            }
+        });
     }
     
     private class GetDataTask extends AsyncTask<String, Integer, String>{
